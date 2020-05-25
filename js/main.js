1d1532ec335b1e685707ececca0ee0f0d965d718
Sahil Kaushal <skaushal91299@gmail.com>
2020-05-25 23:43:50 +0530

    half completed my website

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// NAVBAR
+$('body').css('padding-top', $('.navbar').outerHeight() + 'px')
+
+// detect scroll top or down
+if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function () {
+        scroll_top = $(this).scrollTop();
+        if (scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
+// NAVBAR
+
+
+// POPOVER FUNCTION
+
+// MAIN POPOVER
+$(function () {
+    $('[data-toggle="popover"]').popover()
+});
+// MAIN POPOVER
+
+
+// DISMISS
+$('.resumebtndismiss').popover({
+    trigger: 'hover'
+
+});
+// DISMISS
+
+// CUSTOM POPOVER
+$(function () {
+    $("[rel=popover]").popover({
+        html: true,
+        content: function () {
+            return $("#popcard").html();
+        },
+    });
+});
+// CUSTOM POPOVER
+
+// POPOVER FUNCTION
